bfbd8935d0c658a825df5a01f6c1b6100be5ca00
zamoroz <moras8465132@gmail.com>
2020-03-30 16:33:54 +0700

    5.	С помощью следующего Web-сервиса сформируйте классы объектов для парсинга JSON и добавьте эти классы в пакет model вашего проекта, убрав аннотации.

---
+++ app/src/main/java/com/example/lr7/model/Example.java
@@ -0,0 +1,28 @@
+
+package com.example.lr7.model;
+
+
+
+public class Example {
+
+    private Photos photos;
+
+    private String stat;
+
+    public Photos getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(Photos photos) {
+        this.photos = photos;
+    }
+
+    public String getStat() {
+        return stat;
+    }
+
+    public void setStat(String stat) {
+        this.stat = stat;
+    }
+
+}
---
+++ app/src/main/java/com/example/lr7/model/Photos.java
@@ -0,0 +1,54 @@
+
+package com.example.lr7.model;
+
+import java.util.List;
+
+public class Photos {
+
+    private Integer page;
+    private Integer pages;
+    private Integer perpage;
+    private String total;
+    private List<Photo> photo = null;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public Integer getPerpage() {
+        return perpage;
+    }
+
+    public void setPerpage(Integer perpage) {
+        this.perpage = perpage;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public List<Photo> getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(List<Photo> photo) {
+        this.photo = photo;
+    }
+
+}
